5aedefaa45c7ce7d9a8da6194387c7691114e1af
Samuel <samueljimenez@paucasesnovescifp.cat>
2025-11-30 17:38:33 +0100

    structure 2
    structure + exports + dependencies

---
+++ f2p_games_api/lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:f2p_games_api/providers/games_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final moviesProvider = Provider.of<GamesProvider>(context);
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Home Screen!'),
+      ),
+    );
+  }
+}
---
+++ f2p_games_api/lib/screens/screens.dart
@@ -0,0 +1,2 @@
+
+export 'package:f2p_games_api/screens/home_screen.dart';
